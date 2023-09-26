7865cd5c003294fe2890a24216f4a90031ab79f9
ZIH-JIA HUANG <91250384+jia0268@users.noreply.github.com>
2023-09-26 11:50:40 +0800

    week 03

---
+++ week03_5_leedcode1822.py
@@ -0,0 +1,10 @@
+# leetcode 1822
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        ans = 1
+        for num in nums:
+          ans = ans * num
+
+        if ans>0: return +1
+        if ans<0: return -1
+        if ans==0: return 0
